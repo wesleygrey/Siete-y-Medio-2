0a696acc25fe58ef9e545a83530c65bc56ec3d30
Wesley Hartmann <wesleygrey@g.ucla.edu>
2017-02-07 19:33:07 -0800

    Update siete-y-medio.cpp
    Implemented preliminary file output code

--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -28,6 +28,36 @@ int main(){
 	int dealer_losses = 0;
 	int bet = 0;
 	ofstream gamelog;
+	
+	gamelog.open("gamelog.txt");
+
+
+newRound:
+	if (player.get_rounds() != 0) {
+		gamelog << "-----------------------------------------------" << std::endl << std::endl;
+		gamelog << "Game number: " << player.get_rounds();
+		gamelog << "          " << "Money left: $" << player.get_money()
+			<< std::endl << std::endl;
+		gamelog << "Your cards:" << std::endl;
+		for (size_t pos = 0; pos != my_hand.get_cur_hand().size(); ++pos) {
+			gamelog << "          "
+				<< my_hand.get_cur_hand()[pos].get_spanish_rank() << " de " << my_hand.get_cur_hand()[pos].get_spanish_suit();
+			gamelog << "          "
+				<< "(" << my_hand.get_cur_hand()[pos].get_english_rank() << " of "
+				<< my_hand.get_cur_hand()[pos].get_english_suit() << ")" << std::endl;
+		}
+		gamelog << "Your total: " << my_hand.get_total() << "." << std::endl << std::endl;
+
+		gamelog << "Dealer's cards:" << std::endl;
+		for (size_t pos = 0; pos != dealer_hand.get_cur_hand().size(); ++pos) {
+			gamelog << "          "
+				<< dealer_hand.get_cur_hand()[pos].get_spanish_rank() << " de " << dealer_hand.get_cur_hand()[pos].get_spanish_suit();
+			gamelog << "          "
+				<< "(" << dealer_hand.get_cur_hand()[pos].get_english_rank() << " of "
+				<< dealer_hand.get_cur_hand()[pos].get_english_suit() << ")" << std::endl;
+		}
+		gamelog << "Dealer's total: " << dealer_hand.get_total() << "." << std::endl << std::endl;
+	}
    
    return 0;
 }
